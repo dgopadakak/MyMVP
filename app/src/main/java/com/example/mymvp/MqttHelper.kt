d8ea0060cfab0913028e592f9eaf1ae5f1e1424b
dgopa <dgopadakakv3@gmail.com>
2023-09-18 10:24:35 +0300

    MQTT connection update + MQTT topic subscribe

--- app/src/main/java/com/example/mymvp/MqttHelper.kt
+++ app/src/main/java/com/example/mymvp/MqttHelper.kt
@@ -5,12 +5,23 @@ import io.reactivex.rxjava3.core.Completable
 import org.eclipse.paho.android.service.MqttAndroidClient
 import org.eclipse.paho.client.mqttv3.IMqttActionListener
 import org.eclipse.paho.client.mqttv3.IMqttToken
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions
 import org.eclipse.paho.client.mqttv3.MqttException
 
 class MqttHelper(context: Context)
 {
+    companion object
+    {
+        const val SERVER_URI = "7cc77ccf90ea4aff8def5615d31d2b34.s1.eu.hivemq.cloud"
+        const val USERNAME = "dgopadakak"
+        const val PASSWD = "Ww12345678910"
+        const val CLIENT_ID = "phone"
+        const val TOPIC = "home"
+        const val QOS = 2
+    }
+
     private var mqttAndroidClient: MqttAndroidClient = MqttAndroidClient(context
-        , "7cc77ccf90ea4aff8def5615d31d2b34.s1.eu.hivemq.cloud", "n1")
+        , SERVER_URI, CLIENT_ID)
     var connectionStatus = false
 
     fun connect(): Completable
@@ -18,8 +29,13 @@ class MqttHelper(context: Context)
         return Completable.create { subscriber ->
             try
             {
-                val token = mqttAndroidClient.connect()
-                token.actionCallback = object : IMqttActionListener {
+                val mqttConnectOptions = MqttConnectOptions()
+                mqttConnectOptions.userName = USERNAME
+                mqttConnectOptions.password = PASSWD.toCharArray()
+
+                val token = mqttAndroidClient.connect(mqttConnectOptions)
+                token.actionCallback = object : IMqttActionListener
+                {
                     override fun onSuccess(asyncActionToken: IMqttToken)
                     {
                         connectionStatus = true
@@ -36,6 +52,32 @@ class MqttHelper(context: Context)
             {
                 connectionStatus = false
                 subscriber.onError(e)
+            }
+        }
+    }
+
+    fun subscribeTopic(): Completable
+    {
+        return Completable.create { subscriber ->
+            try
+            {
+                mqttAndroidClient.subscribe(TOPIC, QOS, null, object : IMqttActionListener
+                {
+                    override fun onSuccess(asyncActionToken: IMqttToken)
+                    {
+                        TODO()
+                        // Give your callback on Subscription here
+                    }
+                    override fun onFailure(asyncActionToken: IMqttToken, exception: Throwable)
+                    {
+                        TODO()
+                        // Give your subscription failure callback here
+                    }
+                })
+            } catch (e: MqttException)
+            {
+                TODO()
+                // Give your subscription failure callback here
             }
         }
     }
